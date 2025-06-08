49fe152f8978d5be4c38acc1842c60b7adec3bb1
Dante Rivarola <dante.rivadi@gmail.com>
2025-06-08 18:24:01 -0300

    Fixex no fetching

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ import { Content } from "@/components/Content/Content";
 import { CONTENT_MIN_HEIGHT } from "@/assets/constants";
 import { getAllBlogs } from "@/lib/actions/getAllBlogs";
 
-// Revalidate every 30 seconds instead of on every request
-export const revalidate = 30;
-
 export default async function Home() {
   const data = await getAllBlogs({ page: 1, limit: 10 });
 
